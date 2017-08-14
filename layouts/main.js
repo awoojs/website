829e5c14f1b3f96935dd0fe960e35c0dac16110e
olivia <olivia@fastmail.com>
2017-08-14 23:53:42 +0200

    put some content on there

--- layouts/main.js
+++ layouts/main.js
@@ -1,4 +1,17 @@
 module.exports = (file, files) =>
 `
-<h1>${file.contents}</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>weh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="tachyons.css">
+  </head>
+  <body>
+    <article class="pa3 pa5-ns sans-serif">
+      <h1 class="f3 f2-m f1-l">weh</h1>
+      ${file.contents}
+    </article>
+  </body>
+</html>
 `
--- src/index.md
+++
@@ -1,5 +0,0 @@
----
-layout: main
----
-
-hello world!
